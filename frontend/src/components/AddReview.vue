73342308cb6193e86ca526c766891e5745fb31cb
Schöning <scf@awk.ch>
2021-06-05 23:11:00 +0200

    Update components

---
+++ frontend/src/components/AddReview.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="field">
+      <label class="label">Review Name</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="Review Name"
+          v-model="reviewName"
+        />
+      </div>
+    </div>
+ 
+    <div class="field">
+      <label class="label">Price</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="Price"
+          v-model="reviewPrice"
+        />
+      </div>
+    </div>
+ 
+    <div class="control">
+      <button class="button is-success" @click="saveReview">SAVE</button>
+    </div>
+  </div>
+</template>
+ 
+<script>
+// import axios
+import axios from "axios";
+ 
+export default {
+  name: "AddReview",
+  data() {
+    return {
+      reviewName: "",
+      reviewPrice: "",
+    };
+  },
+  methods: {
+    // Create New review
+    async saveReview() {
+      try {
+        await axios.post("http://localhost:5000/reviews", {
+          review_name: this.reviewName,
+          review_price: this.reviewPrice,
+        });
+        this.reviewName = "";
+        this.reviewPrice = "";
+        this.$router.push("/");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
+ 
+<style>
+</style>
---
+++ frontend/src/components/ReviewList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'Create' }" class="button is-success mt-5"
+      >Add New</router-link
+    >
+    <table class="table is-striped is-bordered mt-2 is-fullwidth">
+      <thead>
+        <tr>
+          <th>Book Name</th>
+          <th>Author</th>
+          <th class="has-text-centered">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.review_id">
+          <td>{{ item.book_name }}</td>
+          <td>{{ item.author }}</td>
+          <td class="has-text-centered">
+            <router-link
+              :to="{ name: 'Edit', params: { id: item.review_id } }"
+              class="button is-info is-small"
+              >Edit</router-link
+            >
+            <a
+              class="button is-danger is-small"
+              @click="deleteReview(item.review_id)"
+              >Delete</a 
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+ 
+<script>
+// import axios
+import axios from "axios";
+ 
+export default {
+  name: "ReviewList",
+  data() {
+    return {
+      items: [],
+    };
+  },
+ 
+  created() {
+    this.getReviews();
+  },
+ 
+  methods: {
+    // Get All Reviews
+    async getReviews() {
+      try {
+        const response = await axios.get("http://localhost:5000/reviews");
+        this.items = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+ 
+    // Delete Review
+    async deleteReview(id) {
+      try {
+        await axios.delete(`http://localhost:5000/reviews/${id}`);
+        this.getReviews();
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
+ 
+<style>
+</style>
